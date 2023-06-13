f9b6a3ea9d2dbe20a9a56b6f03dd13e708716604
s6352410018 <s6335510011@sau.ac.th>
2023-06-13 15:56:27 +0700

    update "Home_Ui"

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -3,10 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:thai_temple_app/utils/app_color.dart';
 
 class HomeUI extends StatefulWidget {
-  const HomeUI({super.key});
+  const HomeUI({Key? key}) : super(key: key);
 
   @override
-  State<HomeUI> createState() => _HomeUIState();
+  _HomeUIState createState() => _HomeUIState();
 }
 
 class _HomeUIState extends State<HomeUI> {
@@ -21,13 +21,82 @@ class _HomeUIState extends State<HomeUI> {
         centerTitle: true,
         backgroundColor: AppColor.primary,
       ),
-      backgroundColor: AppColor.background,
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+            Container(
+              height: 200.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    width: 160.0,
+                    child: Card(
+                      child: Image.network(
+                        'https://thaitemple.sautechnology.com/images/introduction/img1_1.jpg', // Replace with your image URL
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10, // Replace with your item count
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: Icon(Icons.info),
+                    title: Text(
+                        'Temple Information'), // Replace with your temple information
+                    onTap: () {
+                      // Navigate to Detail page
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColor.background,
+        selectedItemColor: AppColor.primary,
+        unselectedItemColor: Colors.grey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.place),
+            label: 'Temple',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Event',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'User',
+          ),
+        ],
       ),
     );
   }
